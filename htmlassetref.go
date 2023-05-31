517de1c1cce031c9b2d6b45488a2cf13672e9423
Ryan Burnette <ryan.burnette@gmail.com>
2023-05-31 10:05:43 -0400

    stub out tag struct

--- htmlassetref.go
+++ htmlassetref.go
@@ -4,6 +4,14 @@ import (
 	"regexp"
 )
 
+// TODO implement this struct as the argument and return for UpdateAssetRefs
+// Tag represents an HTML tag with the content, tag name, and reference.
+type Tag struct {
+	_content string
+	_tag     string
+	ref      string
+}
+
 // UpdateAssetRefs updates asset references in the HTML using the provided callback.
 func UpdateAssetRefs(html string, callback func(ref string) string) string {
 	re := regexp.MustCompile(`(src|href|srcset)=(['"])?([^>\s'"\s]+)(['"\s>])?`)
